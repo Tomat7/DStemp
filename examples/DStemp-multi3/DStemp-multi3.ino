18ee9e0bb42ed814eed8f8f10a910d67e65a22f6
Tomat7 <32893720+Tomat7@users.noreply.github.com>
2018-02-11 13:48:14 +0300

    Update DStemp-multi3.ino

--- examples/DStemp-multi3/DStemp-multi3.ino
+++ examples/DStemp-multi3/DStemp-multi3.ino
@@ -1,22 +1,22 @@
 /*
-   Example for DStemp library on Arduino Nano with THREE sensors (c) Tomat7, Jan 2018
-   
-   Method check() returns NOTHING but does everything:
-   основной метод, делает всё:
-   - проверяет наличие датчика / check for sensor
-   - инициализирует его при необходимости / reinit sensor if necessary
-   - командует датчику начать преобразование температуры / start conversation
-   - получает результат от датчика / collect results
-   
-   Temp return current temperature OR:
-   -99  sensor not found
-   -83  sensor was found but conversation not finished within defined timeout
-   -77  sensor was found and ready but something going wrong
-   -71  sensor was found but CRC error
+  Example for DStemp library on Arduino Nano with THREE sensors (c) Tomat7, Jan 2018
 
-   Connected == 0 значит датчика нет - no sensor found
-   в dsMiilis хранится millis() c момента запроса или крайнего Init()
-   от dsMillis отсчитывается TimeConv - время потраченное на преобразование (DEBUG!)
+  Method check() returns NOTHING but does everything:
+  основной метод, делает всё:
+  - проверяет наличие датчика / check for sensor
+  - инициализирует его при необходимости / reinit sensor if necessary
+  - командует датчику начать преобразование температуры / start conversation
+  - получает результат от датчика / collect results
+
+  Temp return current temperature OR:
+  -99  sensor not found
+  -83  sensor was found but conversation not finished within defined timeout
+  -77  sensor was found and ready but something going wrong
+  -71  sensor was found but CRC error
+
+  Connected == 0 значит датчика нет - no sensor found
+  в dsMiilis хранится millis() c момента запроса или крайнего Init()
+  от dsMillis отсчитывается TimeConv - время потраченное на преобразование (DEBUG!)
 */
 #define SKETCHVERSION __FILE__ " " __DATE__ " " __TIME__
 #define SERIALSPEED 115200
@@ -32,7 +32,7 @@ void setup()
   Serial.begin(SERIALSPEED);
   delay(500);
   Serial.println(SKETCHVERSION);
-  for (int i = 0; i < sensCount; i++) 
+  for (int i = 0; i < sensCount; i++)
   {
     sensor[i].init(DS_CONVTIME);
     String dsInfo = "ID " + String(i, DEC) + "|Connected " + String(sensor[i].Connected, DEC);
@@ -54,6 +54,6 @@ void loop()
                       String(sensor[i].Parasite, DEC) + " | " + String(sensor[i].TimeConv, DEC);
       Serial.println(dsInfo);
     }
-	msReinit = millis();
+    msReinit = millis();
   }
 }
