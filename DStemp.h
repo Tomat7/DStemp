85264b26726b34959970c0b79f6d4a16d909ca57
Tomat7 <star0413@gmail.com>
2018-06-10 23:00:27 +0300

    init1

--- DStemp.h
+++ DStemp.h
@@ -9,9 +9,27 @@
 #include "Arduino.h"
 #include <OneWire.h>
 
-#define LIBVERSION "DStemp_v20180121 on pin "
-#define CONVERSATIONTIME 1000
+#define LIBVERSION "DStemp_v20180531 on pin "
+#define DS_CONVERSATION_TIME 750
 #define INITATTEMPT 2
+
+#define T_MIN -55				// Minimal temperature by DataSheet
+#define T_ERR_CRC -71
+#define T_ERR_OTHER -82 
+#define T_ERR_TIMEOUT -93
+#define T_ERR_FIRST -94			// первая поппытка найти пропавший датчик
+#define T_ERR_SECOND -95		// вторая и финальная попытка
+#define T_ERR_NOTCONNECTED -99	// Not Connected
+
+/*
+#define T_MIN 0				// Minimal temperature by DataSheet
+#define T_ERR_CRC -11
+#define T_ERR_OTHER -22 
+#define T_ERR_TIMEOUT -33
+#define T_ERR_FIRST -44
+#define T_ERR_SECOND -55
+#define T_ERR_NOTCONNECTED -99		// Not Connected
+*/
 
 //#define DSDEBUG true
 //#define DEBUG2
@@ -26,13 +44,16 @@ public:
   // переменные-члены класса и состояние
     //DSThermometer(OneWire *ds);
 	DSThermometer(uint8_t pin);
-    void init(uint16_t convtimeout);
+    void init();
+	void init(uint16_t convtimeout);
+	void init(uint16_t convtimeout, bool printConfig);
     void check();
 	float Temp;
 	unsigned long dsMillis;
 	uint16_t TimeConv;
     bool Connected;   // в нем хранится millis() c момента запроса или попытки инициализации
 	bool Parasite;
+	//String LibVersion;
 	//bool dsDebug;		// если время преобразование не итекло, то выдавать -100 
 						// иначе выдавать температуру крайнего измерения
 private:
