a509add662c0a7060e6b2cc44bbad99e90f3f2ae
Tomat7 <star0413@gmail.com>
2018-11-19 12:23:42 +0300

    CleanUp
    Remove debug option and other cleanup.

--- DStemp.cpp
+++ DStemp.cpp
@@ -16,6 +16,9 @@
 	и от dsMillis отсчитывается msConvTimeout
 */
 
+// See updates on https://github.com/Tomat7/DStemp 
+// Review branches test* for datails, comments and debug options
+
 #include "Arduino.h"
 #include "DStemp.h"
 #include <OneWire.h>
@@ -30,17 +33,13 @@ void DSThermometer::init()
 {
 	init(DS_CONVERSATION_TIME);
 	printConfig();
-	setResolution(12);
+	setResolution(HI_RESOLUTION);
 }
 
 void DSThermometer::init(uint16_t convtimeout)
 {
 	_msConvTimeout = convtimeout;
 	initOW();
-	#ifdef DEBUG2
-	Serial.print(" -init-finised..");
-	Serial.println(millis());
-	#endif
 }
 
 void DSThermometer::control()
@@ -50,14 +49,11 @@ void DSThermometer::control()
 	
 void DSThermometer::check()
 {
-	//#define TIMEISOUT ((millis() - dsMillis) > _msConvTimeout)
 	uint16_t msConvDuration = millis() - dsMillis;	
 	
 	if (Connected && (ds.read_bit() == 1))   	// вроде готов отдать данные
 	{
-		//Serial.print("+");          
 		Temp = askOWtemp();  	// можем ещё получить -71 (CRC error) или -59 (other error)
-		//TimeConv = millis() - dsMillis;
 		TimeConv = msConvDuration;
 	}								
 	else if (msConvDuration > _msConvTimeout)	// подключен, но время на преобразование истекло и не готов отдать данные
@@ -86,18 +82,15 @@ float DSThermometer::askOWtemp()
 		ds.read_bytes(bufData, 9);                 		// чтение памяти датчика, 9 байтов
 		if (OneWire::crc8(bufData, 8) == bufData[8])	// проверка CRC
 		{
-			//Serial.print("+");      // типа всё хорошо!
 			owTemp = (float) ((int) bufData[0] | (((int) bufData[1]) << 8)) * 0.0625; // ХЗ откуда стащил формулу
 		} 
 		else
 		{
-			//Serial.print("*");
 			owTemp = T_ERR_CRC;      // ошибка CRC, вернем -71
 		}
 	}
 	else
 	{
-		//Serial.print("-");        // датчик был, но пропал, (не отдал температуру?) вернем -59,
 		owTemp = T_ERR_OTHER;       // наверное такой косяк тоже может быть, надо разбираться
 	}
 	return owTemp;
@@ -109,10 +102,6 @@ void DSThermometer::requestOW()
 	ds.write(0xCC);
 	ds.write(0x44, Parasite);
 	dsMillis = millis();
-	#ifdef DEBUG3
-	Serial.print("reqOW-stop-");
-	Serial.println(millis());
-	#endif
 	return;
 }
 
@@ -128,14 +117,9 @@ void DSThermometer::initOW()
 	if (ds.read_bit() == 0) Parasite = true;
 	else 					Parasite = false; 
 	// --- END check for Parasite Power
-	//ds.reset_search();
 	dsMillis = millis();
 	if (Connected) requestOW();
 	else Temp = T_ERR_NOSENSOR;
-	#ifdef DEBUG4
-	Serial.print("initOW-stop-");
-	Serial.println(millis());
-	#endif
 	return;
 }
 
@@ -143,6 +127,7 @@ void DSThermometer::printConfig()
 {
 	Serial.print(F(LIBVERSION));
 	Serial.println(_pin);
+	return;
 }
 
 void DSThermometer::setResolution(byte resolution_bits)  // (c) Asif Alam's Blog http://blog.asifalam.com/ds18b20-change-resolution/
@@ -157,15 +142,16 @@ void DSThermometer::setResolution(byte resolution_bits)  // (c) Asif Alam's Blog
 		default: reg_cmd = 0x7F; break;
 	}
 	ds.reset();
-	ds.write(0xCC);  // No address - only one DS on line
-	ds.write(0x4E);  // Write scratchpad command
-	ds.write(0);     // TL data
-	ds.write(0);     // TH data
-	ds.write(reg_cmd); // Configuration Register (resolution) 7F=12bits 5F=11bits 3F=10bits 1F=9bits
-	ds.reset();      // This "reset" sequence is mandatory
+	ds.write(0xCC);  	// No address - only one DS on line
+	ds.write(0x4E);  	// Write scratchpad command
+	ds.write(0);     	// TL data
+	ds.write(0);     	// TH data
+	ds.write(reg_cmd); 	// Configuration Register (resolution) 7F=12bits 5F=11bits 3F=10bits 1F=9bits
+	ds.reset();      	// This "reset" sequence is mandatory
 	ds.write(0xCC);
-	ds.write(0x48, Parasite);  // Copy Scratchpad command
-	delay(20); 	// added 20ms delay to allow 10ms long EEPROM write operation (DallasTemperature)
-	if (Parasite) delay(10); // 10ms delay
+	ds.write(0x48, Parasite);  	// Copy Scratchpad command
+	delay(20); 			// added 20ms delay to allow 10ms long EEPROM write operation (DallasTemperature)
+	if (Parasite) delay(10); 	// 10ms delay
 	ds.reset();
+	return;
 }
--- DStemp.h
+++ DStemp.h
@@ -1,8 +1,13 @@
 /*
   DSThermometer.cpp - Library to operate with DS18B20
   Created by Tomat7, October 2017, 2018.
+  
+  See updates on https://github.com/Tomat7/DStemp 
+  Review branches test* for datails, comments and debug options
+  
   The code of setResolution() from http://blog.asifalam.com/ds18b20-change-resolution/ (c) Asif Alam
 */
+
 #ifndef DStemp_h
 #define DStemp_h
 
@@ -11,8 +16,7 @@
 
 #define LIBVERSION "DStemp_v20181119 on pin "
 #define DS_CONVERSATION_TIME 750
-//#define DS_SET_HI_RESOLUTION
-//#define INITATTEMPT 2
+#define HI_RESOLUTION 12
 
 #define T_MIN -55			// Minimal temperature by DataSheet
 #define T_ERR_OTHER -59 	// sensor was found but something going wrong during conversation (rare)
@@ -20,33 +24,15 @@
 #define T_ERR_TIMEOUT -82	// sensor was found but conversation not finished within defined timeout (may be)
 #define T_ERR_NOSENSOR -99	// Sensor Not Connected (not found)
 
-/* !! OLD data !!
-#define T_MIN 0				// Minimal temperature by DataSheet
-#define T_ERR_CRC -11
-#define T_ERR_OTHER -22 
-#define T_ERR_TIMEOUT -33
-#define T_ERR_FIRST -44
-#define T_ERR_SECOND -55
-#define T_ERR_NOTCONNECTED -99		// Not Connected
-*/
-
-//#define DSDEBUG true
-//#define DEBUG2
-//#define DEBUG3
-//#define DEBUG4
 
 class DSThermometer
 {
 public:
-  // переменные - члены класса »нициализируютс¤ `при запуске
-  // конструктор создает экземпляр Thermometer и инициализирует 
-  // переменные-члены класса и состояние
-    //DSThermometer(OneWire *ds);
+
 	DSThermometer(uint8_t pin);
     void init();
 	void init(uint16_t convtimeout);
-	//void init(uint16_t convtimeout, bool printConfig);
-	//void init(uint16_t convtimeout, bool printConfig, bool setHiRes);
+
 	void check();
 	void control();
 	void printConfig();
@@ -57,14 +43,11 @@ public:
 	uint16_t TimeConv;
     bool Connected;   // в нем хранится millis() c момента запроса или попытки инициализации
 	bool Parasite;
-	//String LibVersion;
-	//bool dsDebug;		// если время преобразование не итекло, то выдавать -100 
-						// иначе выдавать температуру крайнего измерения
+
 private:
     //OneWire *_ds;
 	OneWire ds;
 	byte _pin;
-    //float _temperature;
     uint16_t _msConvTimeout;
     float askOWtemp();
 	void requestOW();
