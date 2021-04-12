a53c1d5f46f1b76c5fe082729b19e21a217f6ee7
Tomat7 <star0413@gmail.com>
2021-04-12 13:52:13 +0300

    Add adjust() and writeCorrection()
    TH and TL registers may keep correction.

--- DStemp.cpp
+++ DStemp.cpp
@@ -1,10 +1,10 @@
 /*
-	DSThermometer.cpp - Library to operate with DS18B20
-	Created by Tomat7. October 2017, 2018.
+	DStemp.cpp - Library to operate with DS18B20
+	Created by Tomat7. October 2017, 2018, 2021.
 */
 /*
 	check() returns NOTHING!
-	only check for temperature changes from OneWire sensor
+	only check for temperature changes from OneWire sensor and update Temp
 	
 	-99  - sensor not found
 	-82  - sensor was found but conversation not finished within defined timeout (may be)
@@ -22,6 +22,18 @@
 #include "Arduino.h"
 #include "DStemp.h"
 #include <OneWire.h>
+
+#if defined(__AVR__)
+	#define IF_AVR(...) (__VA_ARGS__)
+	#else
+	#define IF_AVR(...)
+#endif
+
+#if defined(ESP32) || defined(ESP8266)
+	#define IF_ESP(...) (__VA_ARGS__)
+	#else
+	#define IF_ESP(...)
+#endif
 
 // Scratchpad locations. Thanks to Miles Burton <miles@mnetcs.com> for DallasTemperature library
 #define TEMP_LSB        0
@@ -41,8 +53,7 @@
 #define WRITESCRATCH    0x4E  // Write to scratchpad
 #define RECALLSCRATCH   0xB8  // Recall from EEPROM to scratchpad
 #define READPOWERSUPPLY 0xB4  // Determine if device needs parasite power
-#define ONLYONEDSDEVICE 0xCC  // Only ONE sensor on wire
-
+#define SKIP_ROM 		0xCC  // Only ONE sensor on wire
 
 
 DSThermometer::DSThermometer(uint8_t pin):ds(pin) 
@@ -52,15 +63,15 @@ DSThermometer::DSThermometer(uint8_t pin):ds(pin)
 
 void DSThermometer::init() 
 {
-	init(DS_CONVERSATION_TIME);
+	init(DS_CONV_TIMEOUT);
 	printConfig();
-	setResolution(HI_RESOLUTION);
+	//setResolution(HI_RESOLUTION);
 }
 
 void DSThermometer::init(uint16_t convtimeout)
 {
 	_msConvTimeout = convtimeout;
-	LibConfig = LIBVERSION + String(_pin);
+	LibConfig = DS_LIBVERSION + String(_pin);
 	initOW();
 }
 
@@ -68,7 +79,7 @@ void DSThermometer::control()
 {
 	check();
 }
-	
+
 void DSThermometer::check()
 {
 	uint16_t msConvDuration = millis() - dsMillis;	
@@ -76,15 +87,16 @@ void DSThermometer::check()
 	if (Connected && (ds.read_bit() == 1))   	// вроде готов отдать данные
 	{
 		Temp = askOWtemp();  	// можем ещё получить -71 (CRC error) или -59 (other error)
+		//if (true?) adjust();
 		TimeConv = msConvDuration;
 	}								
 	else if (msConvDuration > _msConvTimeout)	// подключен, но время на преобразование истекло и не готов отдать данные
 	{	
-		Temp = T_ERR_TIMEOUT;	// датчик был, но оторвали на ходу или не успел - косяк короче: -82
+		Temp = DS_T_ERR_TIMEOUT;	// датчик был, но оторвали на ходу или не успел - косяк короче: -82
 	} 
 	else return;
 	
-	if (Temp > T_MIN) requestOW();
+	if (Temp > DS_T_MIN) requestOW();
 	else initOW();
 	
 	return;
@@ -96,38 +108,52 @@ float DSThermometer::askOWtemp()
 	byte present = 0;
 	byte bufData[9]; // буфер данных
 	float owTemp;
+	int16_t rawTemp;
 	
 	present = ds.reset();
 	if (present)
 	{
-		ds.write(ONLYONEDSDEVICE);
+		ds.write(SKIP_ROM);
 		ds.write(READSCRATCH);                     		// Read Scratchpad
 		ds.read_bytes(bufData, 9);                 		// чтение памяти датчика, 9 байтов
 		if (OneWire::crc8(bufData, 8) == bufData[SCRATCHPAD_CRC])	// проверка CRC
 		{
-#if defined(__AVR__)
-			owTemp = (float) ((int) bufData[0] | (((int) bufData[1]) << 8)) * 0.0625; // ХЗ откуда стащил формулу
-#elif defined(ESP32) || defined(ESP8266)
-			int16_t rawTemp = (((int16_t) bufData[TEMP_MSB]) << 11) | (((int16_t) bufData[TEMP_LSB]) << 3);
-			owTemp = (float) rawTemp * 0.0078125f;  // стащил из DallasTemperature
-#endif
+			//owTemp = (float) ((int) bufData[0] | (((int) bufData[1]) << 8)) * 0.0625; // ХЗ откуда стащил формулу
+			rawTemp = ((int16_t) bufData[TEMP_MSB] << 8) | ((int16_t) bufData[TEMP_LSB]);
+			THreg = (int8_t)bufData[HIGH_ALARM_TEMP];
+			TLreg = (int8_t)bufData[LOW_ALARM_TEMP];
+			CFGreg = (int8_t)bufData[CONFIGURATION];
+			owTemp = (float)rawTemp * 0.0625;	// стащил из примера OneWire
+			//rawTemp = (~rawTemp + 1);
 		} 
 		else
 		{
-			owTemp = T_ERR_CRC;      // ошибка CRC, вернем -71
+			owTemp = DS_T_ERR_CRC;      // ошибка CRC, вернем -71
 		}
 	}
 	else
 	{
-		owTemp = T_ERR_OTHER;       // наверное такой косяк тоже может быть, надо разбираться
+		owTemp = DS_T_ERR_OTHER;       // наверное такой косяк тоже может быть, надо разбираться
 	}
 	return owTemp;
+}
+
+void DSThermometer::adjust()
+{
+//	if (rawT < 0) {	rawT = (~rawT + 1) | 0b1000000000000000; }
+
+	if (Temp > DS_T_CORR)
+	{ 
+		Temp *= (1 + (float)THreg * 0.001);	// коррекция пропорциональная
+		Temp += (float)TLreg * 0.01; 		// коррекция сдвиг
+	}
+	return;
 }
 
 void DSThermometer::requestOW()
 {
 	ds.reset();
-	ds.write(ONLYONEDSDEVICE);
+	ds.write(SKIP_ROM);
 	ds.write(STARTCONVO, Parasite);
 	dsMillis = millis();
 	return;
@@ -138,49 +164,68 @@ void DSThermometer::initOW()
 	byte addr[8];
 	ds.reset_search();
 	Connected = (ds.search(addr));
-	// --- check for Parasite Power
+	
+	// check for Parasite Power
 	ds.reset();	
-	ds.write(ONLYONEDSDEVICE);
+	ds.write(SKIP_ROM);
 	ds.write(READPOWERSUPPLY);
 	if (ds.read_bit() == 0) Parasite = true;
 	else 					Parasite = false; 
-	// --- END check for Parasite Power
+	// END check for Parasite Power
+	
 	dsMillis = millis();
-	if (Connected) requestOW();
-	else Temp = T_ERR_NOSENSOR;
+	
+	if (Connected)
+	{
+		requestOW();
+	}
+	else 
+	{
+		Temp = DS_T_ERR_NOSENSOR;
+		THreg = 0;
+		TLreg = 0;
+		CFGreg = 0;
+	}
+	
 	return;
 }
 
 void DSThermometer::printConfig()
 {
 	Serial.println(LibConfig);
-	//Serial.print(F(LIBVERSION));
+	//Serial.print(F(DS_LIBVERSION));
 	//Serial.println(_pin);
 	return;
 }
 
-void DSThermometer::setResolution(byte resolution_bits)  // (c) Asif Alam's Blog http://blog.asifalam.com/ds18b20-change-resolution/
+bool DSThermometer::writeCorrection(byte _bits, int8_t _th, int8_t _tl)  // (c) Asif Alam's Blog http://blog.asifalam.com/ds18b20-change-resolution/
 {
-	byte reg_cmd;
-	switch (resolution_bits) 
+	if ((Connected) && (Temp > DS_T_MIN))
 	{
-		case 9: reg_cmd = 0x1F; break;
-		case 10: reg_cmd = 0x3F; break;
-		case 11: reg_cmd = 0x5F; break;
-		case 12: reg_cmd = 0x7F; break;
-		default: reg_cmd = 0x7F; break;
-	}
-	ds.reset();
-	ds.write(ONLYONEDSDEVICE);  	// No address - only one DS on line
-	ds.write(WRITESCRATCH);  	// Write scratchpad command
-	ds.write(0);     	// TL data
-	ds.write(0);     	// TH data
-	ds.write(reg_cmd); 	// Configuration Register (resolution) 7F=12bits 5F=11bits 3F=10bits 1F=9bits
-	ds.reset();      	// This "reset" sequence is mandatory
-	ds.write(ONLYONEDSDEVICE);
-	ds.write(COPYSCRATCH, Parasite);  	// Copy Scratchpad command
-	delay(20); 			// added 20ms delay to allow 10ms long EEPROM write operation (DallasTemperature)
-	if (Parasite) delay(10); 	// 10ms delay
-	ds.reset();
-	return;
+		byte reg_cmd;
+		switch (_bits) 
+		{
+			case 9: reg_cmd = 0x1F; break;
+			case 10: reg_cmd = 0x3F; break;
+			case 11: reg_cmd = 0x5F; break;
+			case 12: reg_cmd = 0x7F; break;
+			default: reg_cmd = 0x7F; break;
+		}
+		ds.reset();
+		ds.write(SKIP_ROM);  	// No address - only one DS on line
+		ds.write(WRITESCRATCH);  	// Write scratchpad command
+		ds.write(_th);     	// TH data
+		ds.write(_tl);     	// TL data
+		ds.write(reg_cmd); 	// Configuration Register (resolution) 7F=12bits 5F=11bits 3F=10bits 1F=9bits
+		ds.reset();      	// This "reset" sequence is mandatory
+		
+		ds.write(SKIP_ROM);
+		ds.write(COPYSCRATCH, Parasite);  	// Copy Scratchpad command
+		delay(20); 			// added 20ms delay to allow 10ms long EEPROM write operation (DallasTemperature)
+		
+		if (Parasite) delay(10); 	// 10ms delay
+		ds.reset();
+		
+		return true;
+	} else return false;
 }
--- DStemp.h
+++ DStemp.h
@@ -1,11 +1,11 @@
 /*
-  DSThermometer.cpp - Library to operate with DS18B20
-  Created by Tomat7, October 2017, 2018.
+  DStemp.cpp - Library to operate with DS18B20
+  Created by Tomat7, October 2017, 2018, 2021.
   
   See updates on https://github.com/Tomat7/DStemp 
   Review branches test* for datails, comments and debug options
   
-  The code of setResolution() from http://blog.asifalam.com/ds18b20-change-resolution/ (c) Asif Alam
+  The code of setCorrection() from http://blog.asifalam.com/ds18b20-change-resolution/ (c) Asif Alam
 */
 
 #ifndef DStemp_h
@@ -14,20 +14,20 @@
 #include "Arduino.h"
 #include <OneWire.h>
 
-#ifdef LIBVERSION
-#undef LIBVERSION
+#ifdef DS_LIBVERSION
+#undef DS_LIBVERSION
 #endif
-#define LIBVERSION "DStemp_v20210117 on pin "
+#define DS_LIBVERSION "DStemp_v20210404 on pin "
 
-
-#define DS_CONVERSATION_TIME 750
+#define DS_CONV_TIMEOUT 750
 #define HI_RESOLUTION 12
 
-#define T_MIN -55			// Minimal temperature by DataSheet
-#define T_ERR_OTHER -59 	// sensor was found but something going wrong during conversation (rare)
-#define T_ERR_CRC -71		// sensor was found but CRC error (often)
-#define T_ERR_TIMEOUT -82	// sensor was found but conversation not finished within defined timeout (may be)
-#define T_ERR_NOSENSOR -99	// Sensor Not Connected (not found)
+#define DS_T_MIN -55			// Minimal temperature by DataSheet
+#define DS_T_ERR_OTHER -59 	// sensor was found but something going wrong during conversation (rare)
+#define DS_T_ERR_CRC -71		// sensor was found but CRC error (often)
+#define DS_T_ERR_TIMEOUT -82	// sensor was found but conversation not finished within defined timeout (may be)
+#define DS_T_ERR_NOSENSOR -99	// Sensor Not Connected (not found)
+#define DS_T_CORR 80			// What temperature start correction (if available on TH & TL)
 
 
 class DSThermometer
@@ -41,13 +41,17 @@ public:
 	void check();
 	void control();
 	void printConfig();
-	void setResolution(byte res_bit);
+	void adjust();
+	bool writeCorrection(byte _bits, int8_t _th, int8_t _tl);
 	
-	float Temp;
+	float Temp = DS_T_MIN;
 	unsigned long dsMillis;
-	uint16_t TimeConv;
-    bool Connected;   // в нем хранится millis() c момента запроса или попытки инициализации
-	bool Parasite;
+	uint16_t TimeConv = DS_CONV_TIMEOUT;
+    bool Connected = false;
+	bool Parasite = false;
+	int8_t THreg = 0;
+	int8_t TLreg = 0;
+	int8_t CFGreg = 0;
 	
 	String LibConfig;
 
@@ -56,8 +60,8 @@ private:
 	OneWire ds;
 	byte _pin;
     uint16_t _msConvTimeout;
-    float askOWtemp();
+	float askOWtemp();
 	void requestOW();
-    void initOW();
+	void initOW();
 };
 #endif
